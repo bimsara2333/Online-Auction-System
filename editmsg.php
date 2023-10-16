dec51147e92ea81a85462ec2b4c16b1cd4fb345c
GithsaraS <Ishancharun@icloud.com>
2023-10-16 21:31:36 +0530

    fix contact us errors

--- editmsg.php
+++ editmsg.php
@@ -15,58 +15,72 @@
     </header>
 
     <div class="centered-form">
-        <h2>Edit Item Details</h2>
+        <h2>Edit Contact Details</h2>
         <br>
+       
         <?php
         include 'connection.php';
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Form submission logic
-            if (isset($_POST['submit'])) {
+            if (isset($_POST['cid'])) {
+                $cid = $_POST['cid'];
                 $cname = $_POST['cname'];
                 $cemail = $_POST['cemail'];
                 $cmessage = $_POST['cmessage'];
-
-                if (!empty($cname) && !empty($cemail) && !empty($cmessage)) {
-                    // Insert the data into the 'contact' table
-                    $sql = "INSERT INTO `contact` (cname, cemail, cmessage) VALUES ('$cname', '$cemail', '$cmessage')";
+                
+                if (empty($cid) || empty($cname) || empty($cemail) || empty($cmessage) ) {
+                    $errorMessage = "All the fields are required";
+                } else {
+                    $sql = "UPDATE contact SET cname ='$cname', cemail ='$cemail', cmessage ='$cmessage' WHERE cid='$cid'";
                     $result = $con->query($sql);
 
-                    if ($result) {
-                        echo "Message sent successfully!";
+                    if (!$result) {
+                        $errorMessage = "Invalid query: " . $con->error;
                     } else {
-                        echo "Error sending message: " . $con->error;
+                        // Redirect after successful update
+                        header("location:viewContact.php");
+                        exit;
                     }
-                } else {
-                    echo "All fields are required!";
                 }
+            } else {
+                $errorMessage = "Item ID is not set in the form.";
             }
-        }
-
-        // Fetch data if contact_id is provided in the URL
-        $contact_id = $_GET['contact_id'];
-        $sql_fetch = "SELECT * FROM `contact` WHERE id = $contact_id";
-        $result_fetch = $con->query($sql_fetch);
-
-        if ($result_fetch->num_rows > 0) {
-            $row = $result_fetch->fetch_assoc();
-            $cname_value = $row['cname'];
-            $cemail_value = $row['cemail'];
-            $cmessage_value = $row['cmessage'];
         } else {
-            echo "Error fetching data: " . $con->error;
+            // Form rendering logic
+            if (!isset($_GET["cid"])) {
+                header("location:viewContact.php");
+                exit;
+            }
+            $cid = $_GET["cid"];
+
+            $sql = "SELECT * FROM contact WHERE cid=$cid";
+            $result = $con->query($sql);
+            $row = $result->fetch_assoc();
+
+            if (!$row) {
+                header("location:viewContact.php");
+                exit;
+            }
+            $cname = $row['cname'];
+            $cemail = $row['cemail'];
+            $cmessage = $row['cmessage'];
+           
         }
         ?>
 
-        <form method="post" action="">
+            <form action="#" method="post">
+
+            <input type="hidden" name="cid" value="<?php echo $cid; ?>">
+
             <label for="cname">Your Name:</label>
-            <input type="text" id="cname" name="cname" value="<?php echo $cname_value; ?>" required><br><br>
+            <input type="text" id="cname" name="cname" value="<?php echo $cname; ?>" required><br><br>
 
             <label for="cemail">Your Email:</label>
-            <input type="email" id="cemail" name="cemail" value="<?php echo $cemail_value; ?>" required><br><br>
+            <input type="email" id="cemail" name="cemail" value="<?php echo $cemail; ?>" required><br><br>
 
             <label for="cmessage">Message:</label>
-            <textarea id="cmessage" name="cmessage" rows="5" required><?php echo $cmessage_value; ?></textarea><br><br>
+            <textarea id="cmessage" name="cmessage" rows="5" required><?php echo $cmessage; ?></textarea><br><br>
 
             <input type="submit" name="submit" value="Submit" class="register_btn">
         </form>
